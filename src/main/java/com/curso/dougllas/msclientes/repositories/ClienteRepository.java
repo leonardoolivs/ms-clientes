1bfdf7ccc5124bf62f187ee68e4eb3b206a93d2f
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-17 17:49:40 -0300

    feat: Adiciona Repository de Cliente

---
+++ src/main/java/com/curso/dougllas/msclientes/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.curso.dougllas.msclientes.repositories;
+
+import com.curso.dougllas.msclientes.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
