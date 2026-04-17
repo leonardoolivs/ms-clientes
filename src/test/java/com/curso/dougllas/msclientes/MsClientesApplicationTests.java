cced0948dd9c9924e95db6438fef36c84b441db3
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-17 02:20:29 -0300

    build: Cria o projeto com Lombok, H2 e Spring Web

---
+++ src/test/java/com/curso/dougllas/msclientes/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.dougllas.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
