9f22138bcc32be63609fe31d8f7c9a9b535a81b3
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-17 22:51:49 -0300

    feat: Coloca validação de existsByCpf em salvar Cliente

--- src/main/java/com/curso/dougllas/msclientes/repositories/ClienteRepository.java
+++ src/main/java/com/curso/dougllas/msclientes/repositories/ClienteRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     Cliente findByCpf(String cpf);
+
+    boolean existsByCpf(String cpf);
 }
--- src/main/java/com/curso/dougllas/msclientes/services/ClienteService.java
+++ src/main/java/com/curso/dougllas/msclientes/services/ClienteService.java
@@ -1,6 +1,8 @@
 package com.curso.dougllas.msclientes.services;
 
 import com.curso.dougllas.msclientes.entities.Cliente;
+import com.curso.dougllas.msclientes.exceptions.ResourceNotFoundException;
+import com.curso.dougllas.msclientes.exceptions.ValidacaoException;
 import com.curso.dougllas.msclientes.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,20 @@ public class ClienteService {
 
     @Transactional
     public Cliente save(Cliente cliente){
+        if(repository.existsByCpf(cliente.getCpf())){
+            throw new ValidacaoException("CPF já cadastrado");
+        }
+
         return repository.save(cliente);
     }
 
     @Transactional(readOnly = true)
     public Cliente findByCpf(String cpf){
-        return repository.findByCpf(cpf);
+        Cliente cliente = repository.findByCpf(cpf);
+        if(cliente == null){
+            throw new ResourceNotFoundException("CPF não encontrado");
+        }
+
+        return cliente;
     }
 }
