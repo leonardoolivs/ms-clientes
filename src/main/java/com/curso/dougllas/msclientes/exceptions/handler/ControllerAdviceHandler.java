6c456d207535cf9f9206a50e00ecd9b6ce39a992
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-17 22:51:14 -0300

    feat: Adiciona erro personalizado para ValidacaoException no ControllerAdviceHandler

--- src/main/java/com/curso/dougllas/msclientes/exceptions/handler/ControllerAdviceHandler.java
+++ src/main/java/com/curso/dougllas/msclientes/exceptions/handler/ControllerAdviceHandler.java
@@ -1,6 +1,7 @@
 package com.curso.dougllas.msclientes.exceptions.handler;
 
 import com.curso.dougllas.msclientes.exceptions.ResourceNotFoundException;
+import com.curso.dougllas.msclientes.exceptions.ValidacaoException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,15 @@ public class ControllerAdviceHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ApiError apiError = new ApiError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(apiError);
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity<ApiError> validation(ValidacaoException e, HttpServletRequest request){
         HttpStatus status = HttpStatus.NOT_FOUND;
 
         ApiError apiError = new ApiError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
