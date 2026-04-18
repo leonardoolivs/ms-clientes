84c90f4f1ad5f1bd7759145d6645784be544a72d
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-17 22:36:38 -0300

    refactor : Refatora o Controller de Cliente para RequestDTO

--- src/main/java/com/curso/dougllas/msclientes/controllers/ClienteController.java
+++ src/main/java/com/curso/dougllas/msclientes/controllers/ClienteController.java
@@ -1,10 +1,15 @@
 package com.curso.dougllas.msclientes.controllers;
 
+import com.curso.dougllas.msclientes.dtos.ClienteRequestDTO;
+import com.curso.dougllas.msclientes.dtos.mapper.ClienteMapper;
 import com.curso.dougllas.msclientes.entities.Cliente;
 import com.curso.dougllas.msclientes.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,14 +19,25 @@ public class ClienteController {
     @Autowired
     private ClienteService service;
 
+    @Autowired
+    private ClienteMapper mapper;
+
     @PostMapping
-    public Cliente save(@RequestBody Cliente cliente){
-        return service.save(cliente);
+    public ResponseEntity<Cliente> save(@RequestBody ClienteRequestDTO dto){
+
+        Cliente cliente = service.save(mapper.toEntity(dto));
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{cpf}").buildAndExpand(cliente.getCpf()).toUri();
+
+        return ResponseEntity.created(uri).body(cliente);
     }
 
     @GetMapping("/{cpf}")
-    public Cliente findByCpf(@PathVariable("cpf") String cpf){
-        return service.findByCpf(cpf);
+    public ResponseEntity<Cliente> findByCpf(@PathVariable("cpf") String cpf){
+
+        Cliente cliente = service.findByCpf(cpf);
+
+        return ResponseEntity.ok(cliente);
     }
 
 }
